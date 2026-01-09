7db0922bac41e9e95b9dae4951fd63041fea96bf
Marcos Andrade <83091836+Marcos-Andrade22@users.noreply.github.com>
2026-01-09 12:28:37 -0300

    Add Pundit authorization to ApplicationController
    Integrated Pundit for authorization by including Pundit::Authorization, added a before_action to authenticate users, and handled Pundit::NotAuthorizedError with a JSON error response.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include Pundit::Authorization
+  before_action :authenticate_user!
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+  def user_not_authorized
+    render json: { error: "Não autorizado" }, status: :forbidden
+  end
 end
